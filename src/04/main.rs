cae44211d1e0410fdc72a4266d50c6cca621717e
Jordi Been <hi@jordibeen.com>
2024-12-04 10:17:04 +0100

    day 04 part 2

--- src/04/main.rs
+++ src/04/main.rs
@@ -7,11 +7,13 @@ fn main() {
 
     let start: Instant = Instant::now();
     println!("pt1: {} (finished in {:.2?})", pt1(&input), start.elapsed());
+
+    let start: Instant = Instant::now();
+    println!("pt2: {} (finished in {:.2?})", pt2(&input), start.elapsed());
 }
 
 fn pt1(input: &str) -> i32 {
     let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-
     let m = grid.len() as i32;
     let n = grid[0].len() as i32;
 
@@ -34,9 +36,9 @@ fn pt1(input: &str) -> i32 {
                     let possibility = (0..4)
                         .filter_map(|c| {
                             if (i + x * c) >= 0
-                                && (i + x * c) <= m - 1
+                                && (i + x * c) < m
                                 && (j + y * c) >= 0
-                                && (j + y * c) <= n - 1
+                                && (j + y * c) < n
                             {
                                 Some(grid[(i + x * c) as usize][(j + y * c) as usize])
                             } else {
@@ -56,14 +58,56 @@ fn pt1(input: &str) -> i32 {
     ans
 }
 
+fn pt2(input: &str) -> i32 {
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let m = grid.len() as i32;
+    let n = grid[0].len() as i32;
+
+    let mut ans = 0;
+    for i in 0..m {
+        for j in 0..n {
+            if grid[i as usize][j as usize] == 'A' {
+                if i - 1 >= 0 && i + 1 < m && j - 1 >= 0 && j + 1 < n {
+                    let diag_1 = format!(
+                        "{}{}{}",
+                        grid[(i - 1) as usize][(j - 1) as usize],
+                        grid[i as usize][j as usize],
+                        grid[(i + 1) as usize][(j + 1) as usize]
+                    );
+                    let diag_2 = format!(
+                        "{}{}{}",
+                        grid[(i + 1) as usize][(j - 1) as usize],
+                        grid[i as usize][j as usize],
+                        grid[(i - 1) as usize][(j + 1) as usize]
+                    );
+
+                    if (diag_1 == "MAS" || diag_1 == "SAM") && (diag_2 == "MAS" || diag_2 == "SAM")
+                    {
+                        ans += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    ans
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn pt1_test() {
-        let input = include_str!("./example_pt1.txt");
+        let input = include_str!("./example.txt");
         let result = pt1(&input);
         assert_eq!(result, 18);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let result = pt2(&input);
+        assert_eq!(result, 9);
     }
 }
