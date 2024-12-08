4a12f6275d6b64d5262401b07d3f808d6ec116f2
Jordi Been <hi@jordibeen.com>
2024-12-08 20:00:01 +0100

    day 08 part 2

--- src/08/main.rs
+++ src/08/main.rs
@@ -6,10 +6,13 @@ fn main() {
     let start: Instant = Instant::now();
 
     let input: &str = include_str!("./input.txt");
-    println!("pt1: {} (finished in {:.2?})", pt1(&input), start.elapsed());
+    let (bounds, antennas) = parse(&input);
+    println!("pt1: {}", pt1(&bounds, &antennas));
+    println!("pt2: {}", pt2(&bounds, &antennas));
+    println!("Execution time: {:.2?}", start.elapsed());
 }
 
-fn pt1(input: &str) -> i32 {
+fn parse(input: &str) -> ((usize, usize), HashMap<char, Vec<(isize, isize)>>) {
     let mut antennas: HashMap<char, Vec<(isize, isize)>> = HashMap::new();
     let grid: Vec<Vec<char>> = input
         .lines()
@@ -30,9 +33,12 @@ fn pt1(input: &str) -> i32 {
         })
         .collect();
 
-    let m = grid.len();
-    let n = grid[0].len();
+    let bounds = (grid.len(), grid[0].len());
 
+    (bounds, antennas)
+}
+
+fn pt1(bounds: &(usize, usize), antennas: &HashMap<char, Vec<(isize, isize)>>) -> i32 {
     let mut antinodes: HashSet<(isize, isize)> = HashSet::new();
     antennas.values().for_each(|locations| {
         locations.iter().for_each(|antenna| {
@@ -42,11 +48,46 @@ fn pt1(input: &str) -> i32 {
                     let antinode = ((antenna.0 + dist.0), (antenna.1 + dist.1));
 
                     if antinode.0 >= 0
-                        && antinode.0 < m as isize
+                        && antinode.0 < bounds.0 as isize
                         && antinode.1 >= 0
-                        && antinode.1 < n as isize
+                        && antinode.1 < bounds.1 as isize
                     {
                         antinodes.insert(antinode);
+                    }
+                }
+            });
+        });
+    });
+
+    antinodes.len() as i32
+}
+
+fn pt2(bounds: &(usize, usize), antennas: &HashMap<char, Vec<(isize, isize)>>) -> i32 {
+    let mut antinodes: HashSet<(isize, isize)> = HashSet::new();
+    antennas.values().for_each(|locations| {
+        locations.iter().for_each(|antenna| {
+            locations.iter().for_each(|other| {
+                if antenna != other {
+                    let dist = (antenna.0 - other.0, antenna.1 - other.1);
+
+                    let mut antinode_1 = ((antenna.0 + dist.0), (antenna.1 + dist.1));
+                    while antinode_1.0 >= 0
+                        && antinode_1.0 < bounds.0 as isize
+                        && antinode_1.1 >= 0
+                        && antinode_1.1 < bounds.1 as isize
+                    {
+                        antinodes.insert(antinode_1);
+                        antinode_1 = ((antinode_1.0 + dist.0), (antinode_1.1 + dist.1));
+                    }
+
+                    let mut antinode_2 = ((antenna.0 - dist.0), (antenna.1 - dist.1));
+                    while antinode_2.0 >= 0
+                        && antinode_2.0 < bounds.0 as isize
+                        && antinode_2.1 >= 0
+                        && antinode_2.1 < bounds.1 as isize
+                    {
+                        antinodes.insert(antinode_2);
+                        antinode_2 = ((antinode_2.0 + dist.0), (antinode_2.1 + dist.1));
                     }
                 }
             });
@@ -62,8 +103,17 @@ mod tests {
 
     #[test]
     fn pt1_test() {
-        let input = include_str!("./example_pt1.txt");
-        let result = pt1(&input);
+        let input = include_str!("./example.txt");
+        let (bounds, antennas) = parse(&input);
+        let result = pt1(&bounds, &antennas);
         assert_eq!(result, 14);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let (bounds, antennas) = parse(&input);
+        let result = pt2(&bounds, &antennas);
+        assert_eq!(result, 34);
     }
 }
