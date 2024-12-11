d5df9c0bb9dfa745bec6aaef264000c5bb8e71aa
Jordi Been <hi@jordibeen.com>
2024-12-11 20:26:46 +0100

    day 11 part 2

--- src/11/main.rs
+++ src/11/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::time::Instant;
 
 fn main() {
@@ -5,39 +6,60 @@ fn main() {
     let start: Instant = Instant::now();
 
     let input: &str = include_str!("./input.txt");
-    let pt1 = pt1(&input);
-    println!("pt1: {}", pt1);
+    let stones = parse(&input);
+    println!("pt1: {}", blink(stones.clone(), 25));
+    println!("pt2: {}", blink(stones.clone(), 75));
     println!("Execution time: {:.2?}", start.elapsed());
 }
 
-fn pt1(input: &str) -> i32 {
-    let mut stones: Vec<i64> = input
+fn parse(input: &str) -> HashMap<i64, i64> {
+    input
         .split_whitespace()
-        .map(|c| c.parse::<i64>().unwrap())
-        .collect();
+        .fold(HashMap::new(), |mut stones, c| {
+            stones
+                .entry(c.parse::<i64>().unwrap())
+                .and_modify(|v| *v += 1)
+                .or_insert(1);
+            stones
+        })
+}
 
-    (0..25).for_each(|_| {
-        let mut i = 0;
-        while i < stones.len() {
-            if stones[i] == 0 {
-                stones[i] = 1;
-                i += 1;
+fn blink(mut stones: HashMap<i64, i64>, times: i32) -> i64 {
+    (0..times).for_each(|_| {
+        let mut next_stones: HashMap<i64, i64> = HashMap::new();
+        for (stone, count) in stones.iter() {
+            if stone == &0 {
+                next_stones
+                    .entry(1)
+                    .and_modify(|v| *v += *count)
+                    .or_insert(*count);
                 continue;
             }
-            let digits = stones[i].to_string();
+
+            let digits = stone.to_string();
             if digits.len() % 2 == 0 {
                 let (l, r) = digits.split_at(digits.len() / 2);
-                stones[i] = l.parse::<i64>().unwrap();
-                stones.insert(i + 1, r.parse::<i64>().unwrap());
-                i += 2;
+                next_stones
+                    .entry(l.parse::<i64>().unwrap())
+                    .and_modify(|v| *v += *count)
+                    .or_insert(*count);
+                next_stones
+                    .entry(r.parse::<i64>().unwrap())
+                    .and_modify(|v| *v += *count)
+                    .or_insert(*count);
                 continue;
             }
-            stones[i] = stones[i] * 2024;
-            i += 1;
+
+            next_stones
+                .entry(stone * 2024)
+                .and_modify(|v| *v += *count)
+                .or_insert(*count);
         }
+
+        stones = next_stones;
     });
 
-    stones.len() as i32
+    stones.values().sum()
 }
 
 #[cfg(test)]
@@ -47,7 +69,16 @@ mod tests {
     #[test]
     fn pt1_test() {
         let input = include_str!("./example.txt");
-        let result = pt1(&input);
+        let stones = parse(&input);
+        let result = blink(stones, 25);
         assert_eq!(result, 55312);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let stones = parse(&input);
+        let result = blink(stones, 75);
+        assert_eq!(result, 65601038650482);
     }
 }
