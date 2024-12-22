0a2b49d4fa5a4076f96b65a0f949db274b9f0248
Jordi Been <hi@jordibeen.com>
2024-12-22 23:39:20 +0100

    day 17 part 2

--- src/17/main.rs
+++ src/17/main.rs
@@ -1,21 +1,23 @@
-use std::time::Instant;
+use std::{collections::VecDeque, time::Instant};
 
 fn main() {
     println!("--- Day 17: Chronospatial Computer ---");
     let start: Instant = Instant::now();
 
     let input: &str = include_str!("./input.txt");
-    println!("pt1: {}", pt1(&input));
+    let (registers, program) = parse(input);
+    println!("pt1: {}", pt1(registers, &program));
+    println!("pt2: {}", pt2(&program));
     println!("Execution time: {:.2?}", start.elapsed());
 }
 
-fn pt1(input: &str) -> String {
-    let (mut registers, program) = input
+fn parse(input: &str) -> ((u64, u64, u64), Vec<u8>) {
+    input
         .split_once("\n\n")
         .map(|(l, r)| {
-            let registers: Vec<u32> = l
+            let registers: Vec<u64> = l
                 .lines()
-                .map(|line| line.split_once(": ").unwrap().1.parse::<u32>().unwrap())
+                .map(|line| line.split_once(": ").unwrap().1.parse::<u64>().unwrap())
                 .collect();
 
             let program: Vec<u8> = r
@@ -29,30 +31,67 @@ fn pt1(input: &str) -> String {
 
             ((registers[0], registers[1], registers[2]), program)
         })
-        .unwrap();
+        .unwrap()
+}
 
+fn pt1(registers: (u64, u64, u64), program: &Vec<u8>) -> String {
+    let output = run_program(registers, program);
+
+    output
+        .iter()
+        .map(|d| d.to_string())
+        .collect::<Vec<String>>()
+        .join(",")
+}
+
+fn pt2(program: &Vec<u8>) -> u64 {
+    let mut queue: VecDeque<(usize, u64)> = VecDeque::from([(0, 0)]);
+
+    let mut ans = 0;
+    while let Some((index, curr_a)) = queue.pop_front() {
+        if index == program.len() {
+            ans = curr_a;
+            break;
+        }
+
+        (0..8).for_each(|i| {
+            let next_a = (curr_a << 3) | i;
+            let output = run_program((next_a, 0, 0), &program.clone());
+            if let Some(out) = output.iter().nth_back(index) {
+                if program[program.len() - 1 - index] == *out {
+                    queue.push_back((index + 1, next_a));
+                }
+            }
+        });
+    }
+
+    ans
+}
+
+fn run_program(mut registers: (u64, u64, u64), program: &[u8]) -> Vec<u8> {
     let mut output: Vec<u8> = vec![];
     let mut instruction_pointer = 0;
+
     while instruction_pointer < program.len() {
         let opcode = program[instruction_pointer];
         let literal_operand = program[instruction_pointer + 1];
-        let combo_operand: u32 = match literal_operand {
-            n if n <= 3 => n as u32,
+        let combo_operand = match literal_operand {
+            n if n <= 3 => n as u64,
             4 => registers.0,
             5 => registers.1,
             6 => registers.2,
-            7 => u32::MAX,
+            7 => u64::MAX,
             _ => unreachable!(),
         };
         let mut instruction_pointer_jumped = false;
 
         match opcode {
             0 => {
-                let div: f32 = (registers.0 / 2_u32.pow(combo_operand)) as f32;
-                registers.0 = div.trunc() as u32;
+                let div: f32 = (registers.0 / 2_u64.pow(combo_operand as u32)) as f32;
+                registers.0 = div.trunc() as u64;
             }
             1 => {
-                registers.1 = registers.1 ^ literal_operand as u32;
+                registers.1 ^= literal_operand as u64;
             }
             2 => {
                 registers.1 = combo_operand % 8;
@@ -64,18 +103,18 @@ fn pt1(input: &str) -> String {
                 }
             }
             4 => {
-                registers.1 = registers.1 ^ registers.2;
+                registers.1 ^= registers.2;
             }
             5 => {
                 output.push((combo_operand % 8) as u8);
             }
             6 => {
-                let div: f32 = (registers.0 / 2_u32.pow(combo_operand)) as f32;
-                registers.1 = div.trunc() as u32;
+                let div: f64 = (registers.0 / 2_u64.pow(combo_operand as u32)) as f64;
+                registers.1 = div.trunc() as u64;
             }
             7 => {
-                let div: f32 = (registers.0 / 2_u32.pow(combo_operand)) as f32;
-                registers.2 = div.trunc() as u32;
+                let div: f64 = (registers.0 / 2_u64.pow(combo_operand as u32)) as f64;
+                registers.2 = div.trunc() as u64;
             }
             _ => unreachable!(),
         }
@@ -86,10 +125,6 @@ fn pt1(input: &str) -> String {
     }
 
     output
-        .iter()
-        .map(|d| d.to_string())
-        .collect::<Vec<String>>()
-        .join(",")
 }
 
 #[cfg(test)]
@@ -98,8 +133,17 @@ mod tests {
 
     #[test]
     fn pt1_test() {
-        let input = include_str!("./example.txt");
-        let result = pt1(&input);
+        let input = include_str!("./example_pt1.txt");
+        let (registers, program) = parse(input);
+        let result = pt1(registers, &program);
         assert_eq!(result, "4,6,3,5,6,3,5,2,1,0".to_string());
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example_pt2.txt");
+        let (_, program) = parse(input);
+        let result = pt2(&program);
+        assert_eq!(result, 117440);
     }
 }
