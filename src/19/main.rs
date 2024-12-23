72060a80474d3f6c8ce0128a4742b43e6382f10f
Jordi Been <hi@jordibeen.com>
2024-12-23 22:47:34 +0100

    day 19 part 2

--- src/19/main.rs
+++ src/19/main.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 use std::time::Instant;
 
 fn main() {
@@ -6,12 +6,13 @@ fn main() {
     let start: Instant = Instant::now();
 
     let input: &str = include_str!("./input.txt");
-    println!("pt1: {}", pt1(&input));
+    let (towels, designs) = parse(&input);
+    println!("pt1: {}", pt1(&towels, &designs));
+    println!("pt2: {}", pt2(&towels, &designs));
     println!("Execution time: {:.2?}", start.elapsed());
 }
-
-fn pt1(input: &str) -> i32 {
-    let (towels, designs) = input
+fn parse(input: &str) -> (Vec<String>, Vec<String>) {
+    input
         .split_once("\n\n")
         .map(|(t, d)| {
             (
@@ -21,8 +22,10 @@ fn pt1(input: &str) -> i32 {
                 d.lines().map(|s| s.to_string()).collect::<Vec<String>>(),
             )
         })
-        .unwrap();
+        .unwrap()
+}
 
+fn pt1(towels: &Vec<String>, designs: &Vec<String>) -> i32 {
     designs
         .iter()
         .filter(|design| {
@@ -43,6 +46,35 @@ fn pt1(input: &str) -> i32 {
         .count() as i32
 }
 
+fn pt2(towels: &Vec<String>, designs: &Vec<String>) -> i64 {
+    let cache = &mut HashMap::new();
+    designs
+        .iter()
+        .map(|design| ways(cache, &towels, design.to_string()))
+        .sum()
+}
+
+fn ways(cache: &mut HashMap<String, i64>, towels: &Vec<String>, leftover: String) -> i64 {
+    if let Some(cached_val) = cache.get(&leftover) {
+        return *cached_val;
+    }
+
+    let mut ans = 0;
+    if leftover.is_empty() {
+        ans = 1;
+    }
+
+    for towel in towels.iter() {
+        if leftover.starts_with(towel) {
+            ans += ways(cache, &towels, leftover.chars().skip(towel.len()).collect());
+        }
+    }
+
+    cache.insert(leftover, ans);
+
+    ans
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -50,7 +82,16 @@ mod tests {
     #[test]
     fn pt1_test() {
         let input = include_str!("./example.txt");
-        let result = pt1(&input);
+        let (towels, designs) = parse(&input);
+        let result = pt1(&towels, &designs);
         assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let (towels, designs) = parse(&input);
+        let result = pt2(&towels, &designs);
+        assert_eq!(result, 16);
     }
 }
