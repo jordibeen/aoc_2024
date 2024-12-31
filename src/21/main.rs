cacf015eafea81f947917c5b2c9465c23598cf72
Jordi Been <hi@jordibeen.com>
2024-12-31 15:26:02 +0100

    day 21 part 2

--- src/21/main.rs
+++ src/21/main.rs
@@ -1,125 +1,180 @@
 use std::collections::HashMap;
 use std::time::Instant;
 
-type Position = (isize, isize);
+#[derive(Hash, Eq, PartialEq, Clone)]
 enum Keypad {
     Numeric,
     Directional,
 }
 
+type Position = (isize, isize);
+
 fn main() {
     println!("--- Day 21: Keypad Conundrum ---");
-    let start: Instant = Instant::now();
+    let source: Instant = Instant::now();
 
+    let (num_pad, dir_pad) = create_pads();
     let input: &str = include_str!("./input.txt");
-    println!("pt1: {}", pt1(&input));
-    println!("Execution time: {:.2?}", start.elapsed());
+    println!("pt1: {}", pt1(&num_pad, &dir_pad, &input));
+    println!("pt2: {}", pt2(&num_pad, &dir_pad, &input));
+    println!("Execution time: {:.2?}", source.elapsed());
 }
 
-fn pt1(input: &str) -> i32 {
-    let numeric_keypad: Vec<Vec<char>> = vec![
+fn create_pads() -> (HashMap<char, Position>, HashMap<char, Position>) {
+    let num_pad: HashMap<char, Position> = vec![
         vec!['7', '8', '9'],
         vec!['4', '5', '6'],
         vec!['1', '2', '3'],
         vec!['#', '0', 'A'],
-    ];
-    let directional_keypad: Vec<Vec<char>> = vec![vec!['#', '^', 'A'], vec!['<', 'v', '>']];
+    ]
+    .iter()
+    .enumerate()
+    .fold(HashMap::new(), |mut num_pad, (y, row)| {
+        row.iter().enumerate().for_each(|(x, c)| {
+            if c != &'#' {
+                num_pad.insert(*c, (y as isize, x as isize));
+            }
+        });
+        num_pad
+    });
 
-    let num_pad: HashMap<char, Position> =
-        numeric_keypad
-            .iter()
-            .enumerate()
-            .fold(HashMap::new(), |mut num_pad, (y, row)| {
-                row.iter().enumerate().for_each(|(x, c)| {
-                    if c != &'#' {
-                        num_pad.insert(*c, (y as isize, x as isize));
-                    }
-                });
-                num_pad
+    let dir_pad: HashMap<char, Position> = vec![vec!['#', '^', 'A'], vec!['<', 'v', '>']]
+        .iter()
+        .enumerate()
+        .fold(HashMap::new(), |mut dir_pad, (y, row)| {
+            row.iter().enumerate().for_each(|(x, c)| {
+                if c != &'#' {
+                    dir_pad.insert(*c, (y as isize, x as isize));
+                }
             });
-    let dir_pad: HashMap<char, Position> =
-        directional_keypad
-            .iter()
-            .enumerate()
-            .fold(HashMap::new(), |mut dir_pad, (y, row)| {
-                row.iter().enumerate().for_each(|(x, c)| {
-                    if c != &'#' {
-                        dir_pad.insert(*c, (y as isize, x as isize));
-                    }
-                });
-                dir_pad
-            });
+            dir_pad
+        });
 
+    (num_pad, dir_pad)
+}
+
+fn pt1(num_pad: &HashMap<char, Position>, dir_pad: &HashMap<char, Position>, input: &str) -> i32 {
+    let mut cache = &mut HashMap::new();
     input
         .lines()
         .map(|line| {
-            let mut code = line.chars().collect::<Vec<char>>();
-            code = get_code(Keypad::Numeric, &num_pad, &dir_pad, code);
+            let min_sequence_length = get_min_length(
+                &mut cache,
+                Keypad::Numeric,
+                &num_pad,
+                &dir_pad,
+                line.chars().collect::<Vec<char>>(),
+                0,
+                3,
+            );
 
-            (0..2).for_each(|_| {
-                code = get_code(Keypad::Directional, &num_pad, &dir_pad, code.clone());
-            });
-
-            line.trim_end_matches("A").parse::<i32>().unwrap() * code.len() as i32
+            line.trim_end_matches("A").parse::<i32>().unwrap() * min_sequence_length as i32
         })
-        .sum::<i32>()
+        .sum()
 }
 
-fn get_code(
+fn pt2(num_pad: &HashMap<char, Position>, dir_pad: &HashMap<char, Position>, input: &str) -> i64 {
+    let mut cache = &mut HashMap::new();
+    input
+        .lines()
+        .map(|line| {
+            let min_sequence_length = get_min_length(
+                &mut cache,
+                Keypad::Numeric,
+                &num_pad,
+                &dir_pad,
+                line.chars().collect::<Vec<char>>(),
+                0,
+                26,
+            );
+
+            line.trim_end_matches("A").parse::<i64>().unwrap() * min_sequence_length as i64
+        })
+        .sum()
+}
+
+fn get_min_length(
+    cache: &mut HashMap<(Keypad, Vec<char>, u8), i64>,
     pad_type: Keypad,
     num_pad: &HashMap<char, Position>,
     dir_pad: &HashMap<char, Position>,
-    characters: Vec<char>,
-) -> Vec<char> {
-    let (initial_pos, keypad) = match pad_type {
-        Keypad::Numeric => ((3, 2), num_pad),
-        Keypad::Directional => ((0, 2), dir_pad),
+    code: Vec<char>,
+    depth: u8,
+    max_depth: u8,
+) -> i64 {
+    if let Some(cached_val) = cache.get(&(pad_type.clone(), code.clone(), depth)) {
+        return *cached_val;
+    }
+
+    if depth == max_depth {
+        return code.len() as i64;
+    }
+
+    let key_pad = match pad_type {
+        Keypad::Numeric => num_pad,
+        Keypad::Directional => dir_pad,
     };
 
-    characters
-        .iter()
-        .fold((vec![], initial_pos), |(mut chars, mut pos), c| {
-            let char_pos = keypad.get(&c).unwrap();
-            let dist = ((char_pos.0 - pos.0), (char_pos.1 - pos.1));
+    let mut prev = 'A';
+    let mut min_length = 0;
+    for c in &code {
+        min_length += get_min_length(
+            cache,
+            Keypad::Directional,
+            num_pad,
+            dir_pad,
+            get_sequence(key_pad, prev, *c),
+            depth + 1,
+            max_depth,
+        );
+        prev = *c;
+    }
 
-            let horizontal_moves = match dist.1.cmp(&0) {
-                std::cmp::Ordering::Greater => vec!['>'; dist.1 as usize],
-                std::cmp::Ordering::Less => vec!['<'; dist.1.abs() as usize],
-                std::cmp::Ordering::Equal => vec![],
-            };
-            let vertical_moves = match dist.0.cmp(&0) {
-                std::cmp::Ordering::Greater => vec!['v'; dist.0 as usize],
-                std::cmp::Ordering::Less => vec!['^'; dist.0.abs() as usize],
-                std::cmp::Ordering::Equal => vec![],
-            };
+    cache.insert((pad_type.clone(), code.clone(), depth), min_length);
 
-            if dist.0 > 0 && keypad.values().any(|v| v == &(char_pos.0, pos.1)) {
-                chars.extend(vertical_moves);
-                chars.extend(horizontal_moves);
-                chars.push('A');
-                pos = *char_pos;
-                return (chars, pos);
-            }
+    min_length
+}
 
-            if keypad.values().any(|v| v == &(pos.0, char_pos.1)) {
-                chars.extend(horizontal_moves);
-                chars.extend(vertical_moves);
-                chars.push('A');
-                pos = *char_pos;
-                return (chars, pos);
-            }
+fn get_sequence(key_pad: &HashMap<char, Position>, source: char, target: char) -> Vec<char> {
+    let mut sequence = vec![];
 
-            if keypad.values().any(|v| v == &(char_pos.0, pos.1)) {
-                chars.extend(vertical_moves);
-                chars.extend(horizontal_moves);
-                chars.push('A');
-                pos = *char_pos;
-                return (chars, pos);
-            }
+    let source_pos = key_pad.get(&source).unwrap();
+    let target_pos = key_pad.get(&target).unwrap();
+    let dist = ((target_pos.0 - source_pos.0), (target_pos.1 - source_pos.1));
 
-            (chars, pos)
-        })
-        .0
+    let horizontal_moves = match dist.1.cmp(&0) {
+        std::cmp::Ordering::Greater => vec!['>'; dist.1 as usize],
+        std::cmp::Ordering::Less => vec!['<'; dist.1.abs() as usize],
+        std::cmp::Ordering::Equal => vec![],
+    };
+    let vertical_moves = match dist.0.cmp(&0) {
+        std::cmp::Ordering::Greater => vec!['v'; dist.0 as usize],
+        std::cmp::Ordering::Less => vec!['^'; dist.0.abs() as usize],
+        std::cmp::Ordering::Equal => vec![],
+    };
+
+    if dist.1 > 0 && key_pad.values().any(|v| v == &(target_pos.0, source_pos.1)) {
+        sequence.extend(vertical_moves);
+        sequence.extend(horizontal_moves);
+        sequence.push('A');
+        return sequence;
+    }
+
+    if key_pad.values().any(|v| v == &(source_pos.0, target_pos.1)) {
+        sequence.extend(horizontal_moves);
+        sequence.extend(vertical_moves);
+        sequence.push('A');
+        return sequence;
+    }
+
+    if key_pad.values().any(|v| v == &(target_pos.0, source_pos.1)) {
+        sequence.extend(vertical_moves);
+        sequence.extend(horizontal_moves);
+        sequence.push('A');
+        return sequence;
+    }
+
+    sequence
 }
 
 #[cfg(test)]
@@ -129,7 +184,8 @@ mod tests {
     #[test]
     fn pt1_test() {
         let input = include_str!("./example.txt");
-        let result = pt1(&input);
+        let (num_pad, dir_pad) = create_pads();
+        let result = pt1(&num_pad, &dir_pad, &input);
         assert_eq!(result, 126384);
     }
 }
